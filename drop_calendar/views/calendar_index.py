477d7fd49be418f646d2b3aa801b32facd11f563
Dipto K Kundu <dkkundu00@gmail.com>
2021-06-29 11:37:00 +0600

    add Login Url

--- drop_calendar/views/calendar_index.py
+++ drop_calendar/views/calendar_index.py
@@ -1,5 +1,17 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import (
+    UserPassesTestMixin, LoginRequiredMixin, PermissionRequiredMixin
+)
 
 
-class CalendarIndex(TemplateView):
+class CalendarIndex(
+    UserPassesTestMixin,
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+    TemplateView
+):
     template_name = 'drop_calendar/calender_index.html'
+
+    def test_func(self):
+        """Tests if the user is active"""
+        return self.request.user.is_active  # any active user
